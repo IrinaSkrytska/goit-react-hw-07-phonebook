15ae3d8d43db3df3546cca4dac900ecd557ff826
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-08-27 17:42:34 +0300

    adds loader

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,6 +3,7 @@ import Contact from 'components/Contact/Contact';
 import css from './ContactList.module.css';
 import { useSelector } from 'react-redux';
 import { useFetchContactsQuery } from 'redux/phonebookSlice';
+import Loader from 'components/Loader';
 
 const ContactList = () => {
   const { data, isLoading } = useFetchContactsQuery();
@@ -18,17 +19,20 @@ const ContactList = () => {
   };
 
   return (
-    <ul className={css.contact__list}>
-      {data && !isLoading && checkedContacts().length > 0 ? (
-        checkedContacts().map(({ id, name, phone }) => (
-          <li key={id} className={css.contact__list__item}>
-            <Contact id={id} name={name} number={phone} />
-          </li>
-        ))
-      ) : (
-        <p className={css.contacts__message}>No contacts are found</p>
-      )}
-    </ul>
+    <>
+      {isLoading && <Loader />}
+      <ul className={css.contact__list}>
+        {data && !isLoading && checkedContacts().length > 0 ? (
+          checkedContacts().map(({ id, name, phone }) => (
+            <li key={id} className={css.contact__list__item}>
+              <Contact id={id} name={name} number={phone} />
+            </li>
+          ))
+        ) : (
+          <p className={css.contacts__message}>No contacts are found</p>
+        )}
+      </ul>
+    </>
   );
 };
 
